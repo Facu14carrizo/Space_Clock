4844f6e6049688cc3d5ec828a74e7c07e2d8ad5b
Facu14Carrizo <you@example.com>
2024-08-28 21:05:27 -0300

    Agrego Estrellas y planetas

--- app.js
+++ app.js
@@ -14,3 +14,36 @@ function currenTime() {
 }
 
 setInterval(currenTime, 1000);
+
+function createStars(numStars) {
+    const body = document.body;
+
+    for (let i = 0; i < numStars; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+
+        // Establecer tamaño y posición aleatoria
+        const size = Math.random() * 3 + 1; // Tamaño entre 1 y 4px
+        star.style.width = `${size}px`;
+        star.style.height = `${size}px`;
+        star.style.top = `${Math.random() * 100}vh`; // Posición vertical
+        star.style.left = `${Math.random() * 100}vw`; // Posición horizontal
+
+        body.appendChild(star);
+    }
+}
+
+// Llama a la función para crear 100 estrellas
+createStars(900);
+
+const watch = document.getElementById('watch');
+
+watch.addEventListener('mouseover', () => {
+    watch.classList.add('distorted');
+    // Muestra información adicional
+});
+
+watch.addEventListener('mouseout', () => {
+    watch.classList.remove('distorted');
+    // Oculta información adicional
+});
